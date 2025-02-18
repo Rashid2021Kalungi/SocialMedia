e915809143d0b21ce30e27c9a1b29a36c4a355a6
Rashid2021Kalungi <kalungirashid2021@gmail.com>
2025-02-18 17:53:23 +0300

    Layout done

--- src/components/feedbar/Feedbar.jsx
+++ src/components/feedbar/Feedbar.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import './feedbar.css'
+import Share from '../share/Share'
+import Post from '../Post/Post'
 export default function Feedbar() {
   return (
-    <div className='feed'>Feed</div>
+    <div className='feed'>
+      <div className="feedWrapper">
+        <Share/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+      </div>
+    </div>
   )
 }
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,6 +2,59 @@ import React from 'react'
 import './rightbar.css'
 export default function Rightbar() {
   return (
-    <div className='rightbar'>Right</div>
+    <div className='rightbar'>
+      <div className="rightBarWrapper">
+        <div className="birthdayContainer">
+          <img className='birthdayImg' src="/assets/gift.png" alt="" />
+          <span className="birthdayText"><b>Rkman </b>and <b>5 other friends</b> have birthday today</span>
+        </div>
+        <img className="rightbarAd" src='/assets/ad.jpg' alt=''/>
+        <h1 className="rightbarTitle">Online Friends</h1>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img  className='rightbarProfileImg' src="/assets/person/person3.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Tom Cruz</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img  className='rightbarProfileImg' src="/assets/person/person3.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Tom Cruz</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img  className='rightbarProfileImg' src="/assets/person/person3.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Tom Cruz</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img  className='rightbarProfileImg' src="/assets/person/person3.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Tom Cruz</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img  className='rightbarProfileImg' src="/assets/person/person3.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Tom Cruz</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img  className='rightbarProfileImg' src="/assets/person/person3.jpg" alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Tom Cruz</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
